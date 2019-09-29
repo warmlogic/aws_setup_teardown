c21717855442e54051253a5487835f36e36cf986
Matt Mollison <matt.mollison@gmail.com>
2019-09-29 14:16:21 -0700

    update ami query

--- setup_ec2_instance.sh
+++ setup_ec2_instance.sh
@@ -5,7 +5,7 @@
 # example usage: ./setup_ec2_instance.sh t2.micro my-instance-name [128] [ami-name]
 
 # volume size (GB) is an optional third argument (default 128)
-# AMI name is an optional fourth argument (default Ubuntu 16)
+# AMI name is an optional fourth argument (default latest Ubuntu Cloud image owned by Canonical)
 
 if [ -z "$AWS_PROFILE" ] && [ -z "$AWS_DEFAULT_PROFILE" ]; then
     echo "Missing \$AWS_PROFILE or \$AWS_DEFAULT_PROFILE; export before"
@@ -23,22 +23,22 @@ if [ "$#" -eq 4 ]; then
     echo "Using custom AMI: $ami"
     export volumeSize=$3
 elif [ "$#" -eq 3 ]; then
-    # get the ami string for Ubuntu 16
+    # get the ami string for latest Ubuntu Cloud image owned by Canonical
     # https://gist.github.com/vancluever/7676b4dafa97826ef0e9
     export ami=`aws ec2 describe-images \
         --owners 099720109477 \
-        --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-* \
-        --query 'sort_by(Images,&CreationDate)[-1].ImageId'`
-    echo "Using Ubuntu 16 AMI: $ami"
+        --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-* \
+        --query 'reverse(sort_by(Images, &CreationDate))[0].ImageId'`
+    echo "Using Ubuntu AMI: $ami"
     export volumeSize=$3
 elif [ "$#" -eq 2 ]; then
-    # get the ami string for Ubuntu 16
+    # get the ami string for latest Ubuntu Cloud image owned by Canonical
     # https://gist.github.com/vancluever/7676b4dafa97826ef0e9
     export ami=`aws ec2 describe-images \
         --owners 099720109477 \
-        --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-* \
-        --query 'sort_by(Images,&CreationDate)[-1].ImageId'`
-    echo "Using Ubuntu 16 AMI: $ami"
+        --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-* \
+        --query 'reverse(sort_by(Images, &CreationDate))[0].ImageId'`
+    echo "Using Ubuntu AMI: $ami"
     export volumeSize=128
 else
     echo "Arguments not supplied correctly. Exiting."
